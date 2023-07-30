b6987f376f472159b721a10d2cab51e6e396abe2
dafedin <tm.i.sm.master@gmail.com>
2023-07-30 18:46:28 +0300

    :TODO:'s deleted
    from whole project

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -5,8 +5,6 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=20, unique=True)
     phone_number = models.EmailField(unique=True)
-    # been_created :TODO: add field
-    # been_changed
 
     is_auto_generated = models.BooleanField(
         blank=False,
