dab67344667cc074e424336b98331b06db383072
dafedin <tm.i.sm.master@gmail.com>
2023-07-30 19:23:37 +0300

    View and template added
    from generator app as a pattern

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,3 +1,16 @@
-# from django.shortcuts import render
+# For Django
+from django.shortcuts import render
 
-# Create your views here.
+# For contacts handling
+from apps.contacts.services import generate_contacts
+
+
+# Get model for data handling
+def contacts(request):
+    contacts_list = generate_contacts()
+
+    return render(
+        request=request,
+        template_name="contacts/contacts_list.html",
+        context={"users": contacts_list},
+    )
