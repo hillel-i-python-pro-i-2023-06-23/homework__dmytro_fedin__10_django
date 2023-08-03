b0e41b25b20ad37529e750201fb714e012c608ca
dafedin <tm.i.sm.master@gmail.com>
2023-08-04 00:02:01 +0300

    Phone numbers ordered
    in create command

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -6,8 +6,8 @@ from django.core.validators import RegexValidator
 class Contact(models.Model):
     name = models.CharField(max_length=20, unique=True)
     phone_regex = RegexValidator(
-        regex=r"^\+?1?\d{9,15}$",
-        message="Phone number must be entered in the format: '+999999999'",
+        regex=r"^\+?1?[-().\dx]{9,20}$",
+        message="Phone number is invalid",
     )
     phone_number = models.CharField(
         validators=[phone_regex], max_length=17, blank=True
