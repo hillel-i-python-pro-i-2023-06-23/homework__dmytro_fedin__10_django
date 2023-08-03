1e7f5828fbd2e87b68e41c61c2c884fadc973a8e
dafedin <tm.i.sm.master@gmail.com>
2023-08-03 16:54:45 +0300

    Simple view url changed

--- apps/simple_view/views.py
+++ apps/simple_view/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # My Django view
 def index(request, name: str, age: int):
-    return HttpResponse(f"Hello, {name}! My age {age}")
+    # simple_message = f"Hello, {name}! My age {age}"
+    simple_message = "Hello"
+    # return HttpResponse(f"Hello, {name}! My age {age}")
+
+    return render(
+        request=request,
+        template_name="generator/simple_list.html",
+        context={"message": simple_message, "view_name": "simple_view"},
+    )
