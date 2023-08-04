6162fdddcd7bb23191bd37148c4ffcc38bd2aec7
dafedin <tm.i.sm.master@gmail.com>
2023-08-04 11:06:35 +0300

    Empty logo

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "contacts"
 
 urlpatterns = [
-    path("list/", views.ContactsListView.as_view(), name="contacts_list"),
+    path("", views.ContactsListView.as_view(), name="contacts_list"),
     path("create/", views.ContactCreateView.as_view(), name="contacts_create"),
     path(
         "update/<int:pk>/",
